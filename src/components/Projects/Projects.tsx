a06cecd89a6e76850af48504780deda0c964399b
Grant Harrison <grant.tyler.harrison@gmail.com>
2023-09-17 19:38:33 -0500

    Updated routing

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,11 +1,68 @@
-import React, { FC } from 'react';
+import React from 'react'
+import './Projects.css'
 
-interface ProjectsProps {}
+const Projects = () => {
+  return (
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto page-padding max-w-3xl">
+        <div className="text-left">
+          <h1 className="code-h1 text-xl tracking-tight sm:text-6xl">
+            <span className="code animation">Projects</span>
+          </h1>
+          <h2 className="project-title name text-blue-400">
+            <a
+              className="project-title-link"
+              href="https://github.com/grantharrison/personal-website"
+            >
+              Personal Website
+            </a>
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-200">
+            This website itself was actually built and deployed by me! I used
+            React with Typescript to develop this website, then deployed it on
+            Netlify. The purpose of this was to not only show off my frontend
+            development skills but also to have a place to host all my other
+            skills as well. Ask me questions about it or take a look at
+            yourself.
+          </p>
+          <h2 className="project-title name text-blue-400">
+            Kids Code (Private)
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-200">
+            This is a project I am working on that will allow kids at young ages
+            to learn the art of programming, to help develop a better generation
+            of innovators and thinkers. It is built with angular and node, its
+            still a work in progress let me know if you would like to take a
+            look, thanks!
+          </p>
+        </div>
+      </div>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+    </div>
+  )
+}
 
-const Projects: FC<ProjectsProps> = () => (
-  <div data-testid="Projects">
-    Projects Component
-  </div>
-);
-
-export default Projects;
+export default Projects
--- src/components/Resume/Resume.test.tsx
+++ src/components/Resume/Resume.test.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Resume from './Resume';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Resume from './Resume'
 
 describe('<Resume />', () => {
   test('it should mount', () => {
-    render(<Resume />);
-    
-    const resume = screen.getByTestId('Resume');
+    render(<Resume />)
 
-    expect(resume).toBeInTheDocument();
-  });
-});
+    const resume = screen.getByTestId('Resume')
+
+    expect(resume).toBeInTheDocument()
+  })
+})
